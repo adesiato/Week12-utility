9d14b2288a2dd159b9822eddeaeaa20043a8975b
adesiato <acdesiato@gmail.com>
2019-11-19 00:34:47 -0700

    UpdateString

--- Week12-utility.py
+++ Week12-utility.py
@@ -11,3 +11,13 @@ def LoadFile(filename):
     new = contents.split("\n")
     print("OUTPUT %s" % (new))
 
+def UpdateString(string1, string2, index_int):
+    list_1 = []
+    for val in string1:
+        list_1.append(val)
+    list_1[index_int] = string2
+    new_string = ""
+    print("OUTPUT %s" % (new_string.join(list_1)))
+
+
+
