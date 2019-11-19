7c5c187cfcdaf01c8b298f55f2cc968ebaa5c41f
adesiato <acdesiato@gmail.com>
2019-11-19 00:03:22 -0700

    LoadFile

---
+++ Week12-utility.py
@@ -0,0 +1,12 @@
+# Anthony Desiato
+# CSCI 102 - Section A
+# Week 12 - Lab A
+
+def PrintOutput(string):
+    print("OUTPUT %s" % (string))
+
+def LoadFile(filename):
+    bruh = open(filename, "r")
+    contents = bruh.read()
+    new = contents.split("\n")
+    print(new)
